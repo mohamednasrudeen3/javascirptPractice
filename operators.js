1b662b81dd2a8da1554b4767b60a503e7e771efb
mohamednasrudeen3 <mohamednasrudeen21@gmail.com>
2025-09-12 21:37:21 +0530

    increment

---
+++ operators.js
@@ -0,0 +1,20 @@
+// Arithmetic: +, -, *, /, %, ++, --
+// Assignment: =, +=, -=, *=, /=, %=
+// Comparison: ==, ===, !=, !==, >, <, >=, <=
+// Logical: &&, ||, !
+// Bitwise: &, |, ^, ~, <<, >>, >>>
+// Ternary: condition ? expr1 : expr2
+// Type: typeof, instanceof
+
+// let a = 10;
+// let b = a **2;
+// console.log(b);
+
+a = 10
+a%=2
+console.log(a)
+console.log(5/0)
+
+
+
+
